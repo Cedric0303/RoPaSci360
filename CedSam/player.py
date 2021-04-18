15bab881206a31b8833b8af3b3e9a8518797a8b1
Cedric0303 <chenjunli7@gmail.com>
2021-04-18 22:23:13 +0800

    fix adjust list

--- CedSam/player.py
+++ CedSam/player.py
@@ -4,7 +4,7 @@ import numpy as np
 from CedSam.board import Board
 from CedSam.side import Lower, Upper
 from CedSam.token import Rock, Paper, Scissors
-from gametheory2 import solve_game
+from CedSam.gametheory2 import solve_game
 
 class Player:
     def __init__(self, side):
@@ -51,6 +51,13 @@ class Player:
                 q = randrange(-4, 5)
             return ("THROW", token.name.lower(), (r, q))
         else:
+            for token in self.self_tokens:
+                both = [target for target in self.opponent_tokens if isinstance(target, token.enemy)] + [enemy for enemy in self.opponent_tokens if isinstance(enemy, token.avoid)]
+                while both:
+                    opponent = both.pop(0)
+                    best, util_matrix, moves, opp_moves, best_idx, val, sol = self.lookahead(token, opponent, self.self_tokens, self.opponent_tokens, depth=0)
+
+            pass
             # VVV ALGO GOES IN THIS SECTION OF THE CODE VVV
             # slide
             # swing
@@ -113,13 +120,13 @@ class Player:
     # Adjusts a list of tokens to provide updated list
     def adjust_list(self, token, token_list, new_r, new_q):
 
-        token_list.remove(token)
-        if token.name.upper() == 'R':
-            token_list.append(Rock(token.side, new_r, new_q))
-        elif token.name.upper() == 'P':
-            token_list.append(Paper(token.side, new_r, new_q))
-        elif token.name.upper() == 'S':
-            token_list.append(Scissors(token.side, new_r, new_q))
+        token_list[token_list.index(token)].move(new_r, new_q)
+        # if token.name.upper() == 'R':
+        #     token_list.append(Rock(token.side, new_r, new_q))
+        # elif token.name.upper() == 'P':
+        #     token_list.append(Paper(token.side, new_r, new_q))
+        # elif token.name.upper() == 'S':
+        #     token_list.append(Scissors(token.side, new_r, new_q))
 
         return token_list
 
@@ -134,8 +141,8 @@ class Player:
     def build_utility(self, token_considered, enemy_token, self_tokens, opponent_tokens):
         
         util_matrix = list()
-        possible = token_considered.get_adj_hex()
-        enemy_moves = enemy_token.get_adj_hex()
+        possible = token_considered.get_adj_hex(token_considered.r, token_considered.q)
+        enemy_moves = enemy_token.get_adj_hex(enemy_token.r, enemy_token.q)
         
         opp_valid_moves = list()
         my_valid_moves = list()
@@ -156,7 +163,7 @@ class Player:
                         opp_valid_moves.append((opp_r, opp_q))
 
                         # now we have the modified lists of tokens, i.e. a gamestate where two moves have been taken
-                        alive_self, alive_opp = Board.battle(shallow_self, shallow_opp)
+                        alive_self, alive_opp = self.board.battle(shallow_self, shallow_opp)
 
                         row_utility.append(self.simple_eval(token_considered, move_r, move_q, alive_self, alive_opp))
                 
@@ -169,17 +176,11 @@ class Player:
 
     # Returns simple evaluation of player tokens minus opponent tokens
     def simple_eval(self, cur_token, r, q, self_tokens, opponent_tokens):
-        print("target", self.target_eval(cur_token, r, q, opponent_tokens))
-        # print("ally", self.ally_eval(cur_token, r, q, self_tokens))
-        # print("avoid", self.avoid_eval(cur_token, r, q, opponent_tokens))
-        difference = 0
-        difference += self.target_eval(cur_token, r, q, opponent_tokens)
-        difference += self.ally_eval(cur_token, r, q, self_tokens)
-        difference += self.avoid_eval(cur_token, r, q, opponent_tokens)
-        print("diff", difference)
+        difference = self.target_enemy_eval(cur_token, r, q, opponent_tokens)
+        # difference += self.ally_eval(cur_token, r, q, self_tokens)
         return difference
 
-    def target_eval(self, cur_token, r, q, opponent_tokens):
+    def target_enemy_eval(self, cur_token, r, q, opponent_tokens):
         w = 10
         targets = [token for token in opponent_tokens if isinstance(token, cur_token.enemy)]
         distance = [cur_token.manhattan_distance([r, q], [target.r, target.q]) for target in targets]
@@ -191,18 +192,11 @@ class Player:
         team_kill = [(token.r, token.q) for token in self_tokens if isinstance(token, cur_token.enemy) or isinstance(token, cur_token.avoid)]
         return w if (r, q) in team_kill else 0
 
-    def avoid_eval(self, cur_token, r, q, opponent_tokens):
-        w = 1
-        enemies = [token for token in opponent_tokens if isinstance(token, cur_token.avoid)]
-        distance = [cur_token.manhattan_distance([r, q], [enemy.r, enemy.q]) for enemy in enemies]
-        # distance = [dist([r, q], [enemy.r, enemy.q]) for enemy in enemies]
-        return round(w * (min(distance) + 1)) if distance else 0
-
-    def swing_eval(self, cur_token, r, q, self_tokens):
-        w = 2
-        adj = set(cur_token.get_adj_hex(r, q))
-        allies = set([(token.r, token.q) for token in self_tokens])
-        return w if adj.intersection(allies) else 0
+    # def swing_eval(self, cur_token, r, q, self_tokens):
+    #     w = 2
+    #     adj = set(cur_token.get_adj_hex(r, q))
+    #     allies = set([(token.r, token.q) for token in self_tokens])
+    #     return w if adj.intersection(allies) else 0
     
     """
     Carries out the search in a tree of utility matrices to find the best action for our token
@@ -217,13 +211,15 @@ class Player:
         if depth == 2:
             util_matrix, my_moves, opp_moves = self.build_utility(token_considered, target, self_tokens, opponent_tokens)
             sol, val = solve_game(np.array(util_matrix), maximiser=True, rowplayer=True)
-
-            return max(sol) * val
+            sol = sol.tolist()
+            return max(sol) * val, util_matrix, my_moves, opp_moves, sol.index(max(sol)), val, sol
 
         util_matrix, my_moves, opp_moves = self.build_utility(token_considered, target, self_tokens, opponent_tokens)
         sol_best, val_best = solve_game(np.array(util_matrix), maximiser=True, rowplayer=True)
         sol_opp, val_opp = solve_game(np.array(util_matrix), maximiser=False, rowplayer=False)
 
+        sol_best = sol_best.tolist()
+        sol_opp = sol_opp.tolist()
         (best_r, best_q) = my_moves[sol_best.index(max(sol_best))]
         (opp_r, opp_q) = opp_moves[sol_opp.index(max(sol_opp))]
 
